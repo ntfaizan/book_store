57d24f567f16dd998cb39a6cbc8ce134fd2fc7db
ProgrammingWithFaizan <faizan.abbas020@gmail.com>
2023-08-03 20:21:26 +0500

    network fix

--- lib/network/network_client.dart
+++ lib/network/network_client.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 
 class NetworkClient {
   final dio = Dio();
-  static const baseUrl = 'http://mad.codewithfaizan.com/';
+  static const baseUrl = 'http://mad.nsdd.cloud/';
 
   NetworkClient() {
     dio.interceptors.add(
@@ -18,22 +18,66 @@ class NetworkClient {
   }
 
   Future<Response> get(String path,
-      {Map<String, dynamic>? queryParameters}) async {
-    return await dio.get(baseUrl + path, queryParameters: queryParameters);
+      {Map<String, dynamic>? queryParameters, String? token}) async {
+    Map<String, dynamic> headers = {"Accept": "application/json"};
+    if (token != null) {
+      headers.addAll({"Authorization": "Bearer $token"});
+    }
+    return await dio.get(
+      baseUrl + path,
+      queryParameters: queryParameters,
+      options: Options(
+        headers: headers,
+        responseType: ResponseType.json,
+      ),
+    );
   }
 
   Future<Response> delete(String path,
-      {Map<String, dynamic>? queryParameters}) async {
-    return await dio.delete('$baseUrl$path', queryParameters: queryParameters);
+      {Map<String, dynamic>? queryParameters, String? token}) async {
+    Map<String, dynamic> headers = {"Accept": "application/json"};
+    if (token != null) {
+      headers.addAll({"Authorization": "Bearer $token"});
+    }
+    return await dio.delete(
+      '$baseUrl$path',
+      queryParameters: queryParameters,
+      options: Options(
+        headers: headers,
+        responseType: ResponseType.json,
+      ),
+    );
   }
 
   Future<Response> put(String path,
-      {Map<String, dynamic>? queryParameters}) async {
-    return await dio.put('$baseUrl$path', queryParameters: queryParameters);
+      {Map<String, dynamic>? queryParameters, String? token}) async {
+    Map<String, dynamic> headers = {"Accept": "application/json"};
+    if (token != null) {
+      headers.addAll({"Authorization": "Bearer $token"});
+    }
+    return await dio.put(
+      '$baseUrl$path',
+      queryParameters: queryParameters,
+      options: Options(
+        headers: headers,
+        responseType: ResponseType.json,
+      ),
+    );
   }
 
   Future<Response> post(String path,
-      {Map<String, dynamic>? queryParameters}) async {
-    return await dio.post('$baseUrl$path', queryParameters: queryParameters);
+      {Map<String, dynamic>? queryParameters, String? token}) async {
+    Map<String, dynamic> headers = {"Accept": "application/json"};
+    if (token != null) {
+      headers.addAll({"Authorization": "Bearer $token"});
+    }
+    return await dio.post(
+      '$baseUrl$path',
+      queryParameters: queryParameters,
+      options: Options(
+        headers: headers,
+        responseType: ResponseType.json,
+      ),
+    );
   }
 }
